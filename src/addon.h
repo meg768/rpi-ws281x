20e6d427c709384155e744a1573e46388a19c33b
meg768 <meg768@users.noreply.github.com>
2019-05-28 13:34:18 +0200

    -

--- src/addon.h
+++ src/addon.h
@@ -3,17 +3,31 @@
 #define _addon_h
 
 #include <nan.h>
-
 #include <v8.h>
-
+#include <stdexcept>
+#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+#include <sys/time.h>
 #include <string.h>
-
+#include <fcntl.h>
+#include <errno.h>
+#include <signal.h>
+#include <memory.h>
+#include <math.h>
+#include <vector>
+#include <dirent.h>
+#include <getopt.h>
 #include <algorithm>
 
 #include "rpi_ws281x/ws2811.h"
 
+using namespace std;
 
 class Addon {
 
