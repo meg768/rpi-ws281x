9191ec4216eb7a0e078e631ebb7008c8c8948792
meg768 <meg768@users.noreply.github.com>
2019-05-31 17:59:38 +0200

    -

--- examples/running-pixel.js
+++ examples/running-pixel.js
@@ -1,10 +1,11 @@
-var ws211x = require('../index.js');
-
+var ws281x = require('../index.js');
 
 class App {
 
     constructor() {
+        // The number of leds in my strip
         this.leds = 169;
+
         this.offset = 0;
 
         this.run();
@@ -12,17 +13,25 @@ class App {
 
     loop() {
 
+        // Clear all pixels
         for (var i = 0; i < this.leds; i++)
             this.pixels[i] = 0;
 
+        // Set a specific pixel
         this.pixels[this.offset] = 0xFF0000;
+
+        // Move on to next
         this.offset = (this.offset + 1) % this.leds;
 
-        ws211x.render(this.pixels);
+        // Render to strip
+        ws281x.render(this.pixels);
     }
 
     run() {
-        ws211x.configure({leds:this.leds});
+        // Configure ws281x
+        ws281x.configure({leds:this.leds});
+
+        // Loop every 100 ms
         setInterval(this.loop.bind(this), 100);
     }
     
