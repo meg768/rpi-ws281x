424d9dc8df0319865e6e3bbbcdf81a43baed3660
meg768 <meg768@users.noreply.github.com>
2019-06-01 00:12:05 +0200

    -

--- examples/fill-with-color.js
+++ examples/fill-with-color.js
@@ -3,24 +3,37 @@ var ws281x = require('../index.js');
 class Example {
 
     constructor() {
+        this.config = {};
+
         // Number of leds in my strip
-        this.leds = 169;
+        this.config.leds = 169;
+
+        // Use DMA 10 (default 10)
+        this.config.dma = 10;
+
+        // Set full brightness, a value from 0 to 255 (default 255)
+        this.config.brightness = 255;
+
+        // Set the GPIO number to communicate with the Neopixel strip
+        this.config.gpio = 18;
+
+        // The RGB sequence may vary on some strips. Valid values
+        // are "rgb", "rbg", "grb", "gbr", "bgr", "brg" 
+        this.config.strip = 'gbr';
 
         // Configure ws281x
-        ws281x.configure({leds:this.leds});
+        ws281x.configure(this.config);
     }
 
     run() {
-        // Number of leds
-        var leds = this.leds;
-
         // Create an pixel array matching the number of leds
-        var pixels = new Uint32Array(leds);
+        var pixels = new Uint32Array(this.config.leds);
 
         // Create a fill color (R/G/B)
-        var color = (255 << 16) | (0 << 8)| 0;
+        var red = 255, green = 0, blue = 0;
+        var color = (red << 16) | (green << 8)| blue;
 
-        for (var i = 0; i < leds; i++)
+        for (var i = 0; i < this.config.leds; i++)
             pixels[i] = color;
 
         // Render to strip
