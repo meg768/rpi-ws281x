3caade14a9bb7e4798a44449c639811c48b0ac9f
meg768 <meg768@users.noreply.github.com>
2019-06-01 00:45:34 +0200

    -

---
+++ examples/pixel-mapping.js
@@ -0,0 +1,41 @@
+var ws281x = require('../index.js');
+
+class Example {
+
+    constructor() {
+        // Current pixel position
+        this.offset = 0;
+
+        // Set my Neopixel configuration
+        this.config = {};
+
+        this.config.map = 'alternating-matrix';
+        this.config.width = 13;
+        this.config.height = 13;
+
+        
+        // Configure ws281x
+        ws281x.configure(this.config);
+    }
+
+    loop() {
+        var pixels = new Uint32Array(this.config.leds);
+
+        // Set a specific pixel
+        pixels[this.offset] = 0xFF0000;
+
+        // Move on to next
+        this.offset = (this.offset + 1) % this.config.leds;
+
+        // Render to strip
+        ws281x.render(pixels);
+    }
+
+    run() {
+        // Loop every 100 ms
+        setInterval(this.loop.bind(this), 100);
+    }
+};
+
+var example = new Example();
+example.run();
