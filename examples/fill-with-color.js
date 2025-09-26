bc5fe4032dc522e96255765437ba973b74571e1a
meg768 <magnus@egelberg.se>
2025-09-26 23:21:54 +0200

    -

--- examples/fill-with-color.js
+++ examples/fill-with-color.js
@@ -32,9 +32,9 @@ class Example {
 		var pixels = new Uint32Array(this.config.leds);
 
 		// Create a fill color with red/green/blue.
-		var red = 255,
-			green = 0,
-			blue = 0;
+		var red = 255;
+		var green = 0;
+        var blue = 0;
 		var color = (red << 16) | (green << 8) | blue;
 
 		for (var i = 0; i < this.config.leds; i++) pixels[i] = color;
