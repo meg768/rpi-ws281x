71e18bac72af75d3e8975067f160ac4b3c24f578
meg768 <magnus@egelberg.se>
2025-09-26 20:51:35 +0200

    -

--- examples/fill-with-color.js
+++ examples/fill-with-color.js
@@ -4,7 +4,6 @@ class Example {
 
     constructor() {
         this.leds = 169;
-        this.pixels = new Uint32Array(this.leds);
         
         // Configure ws281x
         ws281x.configure({leds:this.leds, stripType : 'grb', dma:10, gpio:18});
@@ -12,16 +11,17 @@ class Example {
     }
 
     run() {
+        let pixels = new Uint32Array(this.leds);
 
         // Create a fill color with red/green/blue.
         var red = 0, green = 255, blue = 0;
         var color = (red << 16) | (green << 8)| blue;
 
         for (var i = 0; i < this.leds; i++)
-            this.pixels[i] = color;
+            pixels[i] = color;
 
         // Render to strip
-        ws281x.render(this.pixels);
+        ws281x.render(pixels);
     }
     
 };
