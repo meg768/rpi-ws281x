61e834fa0fe4c42534bc58cb0e73ba5527357a6b
meg768 <magnus@egelberg.se>
2025-09-26 18:29:01 +0200

    -

--- index.js
+++ index.js
@@ -1,105 +1,100 @@
-var path = require("path");
-var addon = require(path.join(__dirname, "build", "Release", "rpi-ws281x.node"));
+var path = require('path');
+var addon = require(path.join(__dirname, 'build', 'Release', 'rpi-ws281x.node'));
 
 class Module {
-    constructor() {
-        this.map  = undefined;
-        this.leds = undefined;
-    }
+	constructor() {
+		this.map = undefined;
+		this.leds = undefined;
+	}
 
-    configure(options) {
-        var {width, height, map, leds, ...options} = options;
-        
-        if (width != undefined || height != undefined) {
+	configure(options) {
+		var { width, height, map, leds, ...options } = options;
 
-            if (width == undefined) {
-                throw new Error('Must specify width if height is specified.');
-            }
+		if (width != undefined || height != undefined) {
+			if (width == undefined) {
+				throw new Error('Must specify width if height is specified.');
+			}
 
-            if (height == undefined) {
-                throw new Error('Must specify height if width is specified.');
-            }
+			if (height == undefined) {
+				throw new Error('Must specify height if width is specified.');
+			}
 
-            if (leds != undefined) {
-                throw new Error('Please do not specify leds when both width and height are specified.');
-            }
+			if (leds != undefined) {
+				throw new Error('Please do not specify leds when both width and height are specified.');
+			}
 
-            leds = width * height;
+			leds = width * height;
 
-            if (typeof map == 'string') {
-                if (map == 'matrix') {
-                    map = new Uint32Array(width * height);
-        
-                    for (var i = 0; i < map.length; i++) 
-                        map[i] = i;
-        
-                }
-                else if (map == 'alternating-matrix') {
-                    map = new Uint32Array(width * height);
-        
-                    for (var i = 0; i < map.length; i++) {
-                        var row = Math.floor(i / width), col = i % width;
-                
-                        if ((row % 2) === 0) {
-                            map[i] = i;
-                        }
-                        else {
-                            map[i] = (row+1) * width - (col+1);
-                        }
-                    }        
-                }
-            }
-        }
+			if (map) {
+				if (map instanceof Uint32Array) {
+					if (map.length != leds) {
+						throw new Error('Pixel mapping array must be of the same size as the number of leds.');
+					}
+					this.map = map;
+				} else {
+					if (typeof map == 'string') {
+						if (map == 'alternating-matrix') {
+							map = new Uint32Array(width * height);
 
-        // Make sure the number of leds are specified
-        if (leds == undefined) {
-            throw new Error('Number of leds must be defined. Either by leds or by width and height.');
-        }
-    
-        // If no map specified, create a default one...
-        if (map == undefined) {
-            map = new Uint32Array(leds);
+							for (var i = 0; i < map.length; i++) {
+								var row = Math.floor(i / width),
+									col = i % width;
 
-            for (var i = 0; i < leds; i++)
-                map[i] = i;
-        }
-        
-        // Make sure we have a correct instance of pixel mapping
-        if (!(map instanceof Uint32Array))
-            throw new Error('Pixel mapping must be an Uint32Array.');
+								if (row % 2 === 0) {
+									map[i] = i;
+								} else {
+									map[i] = (row + 1) * width - (col + 1);
+								}
+							}
 
-        if (map.length != leds) 
-            throw new Error('Pixel mapping array must be of the same size as the number of leds.');
+							this.map = map;
+						}
+					}
+				}
+			}
+		}
 
-        this.map  = map;
-        this.leds = leds;
+		// Make sure the number of leds are specified
+		if (leds == undefined) {
+			throw new Error('Number of leds must be defined. Either by leds or by width and height.');
+		}
 
-        addon.configure({...options, leds:leds});
-    }
+		this.leds = leds;
 
-    reset() {
-        if (this.leds != undefined) {
-            addon.reset();    
-        }
-    }
+		addon.configure({ ...options, leds: leds });
+	}
 
-    sleep(ms) {
-        addon.sleep(ms);
-    }
+	reset() {
+		if (this.leds != undefined) {
+			addon.reset();
+		}
+	}
 
-    render(pixels) {
-        if (this.map != undefined) {
-            // Convert to Uint32Array if a Buffer
-            if (!(pixels instanceof Uint32Array))
-                throw new Error('Pixels must be of type Uint32Array in render()');
- 
-            if (this.leds != pixels.length)
-                throw new Error('Pixels must be of same length as number of leds in render()');
+	sleep(ms) {
+		addon.sleep(ms);
+	}
 
-            addon.render(pixels);
-        }
-    }
+	render(pixels) {
+		// Convert to Uint32Array if a Buffer
+		if (!(pixels instanceof Uint32Array)) {
+			throw new Error('Pixels must be of type Uint32Array in render()');
+		}
+
+		if (this.leds != pixels.length) {
+			throw new Error('Pixels must be of same length as number of leds in render()');
+		}
+
+		if (this.map) {
+			var mapped = new Uint32Array(this.leds);
+
+			for (var i = 0; i < mapped.length; i++) {
+				mapped[i] = pixels[this.map[i]];
+			}
+			addon.render(mapped);
+		} else {
+			addon.render(pixels);
+		}
+	}
 }
-
 
 module.exports = new Module();
