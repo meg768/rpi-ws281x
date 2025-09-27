9193ebbaae74e9c2d371c7831cb16e53abbda32a
meg768 <magnus@egelberg.se>
2025-09-27 13:03:10 +0200

    -

--- examples/fill-with-color.js
+++ examples/fill-with-color.js
@@ -22,7 +22,7 @@ class Example {
 		// RGBW strips are not currently supported.
 		this.config.stripType = 'grb';
 
-        this.gamma = 0.1;
+        this.config.gamma = 0.1;
 
 		// Configure ws281x
 		ws281x.configure(this.config);
