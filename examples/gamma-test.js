84e7ce5f2ce3c320a6a49a2b98c3d08fa9444354
meg768 <magnus@egelberg.se>
2025-10-06 21:06:00 +0200

    -

--- examples/gamma-test.js
+++ examples/gamma-test.js
@@ -2,7 +2,7 @@ var ws281x = require('../index.js');
 
 let leds = 169;
 
-ws281x.configure({ leds: leds, stripType: 'grb', gpio: 18, dma: 10, brightness: 255, gamma:0.5 });
+ws281x.configure({ leds: leds, stripType: 'grb', gpio: 18, dma: 10, brightness: 255, gamma:1 });
 
 // Create my pixels
 var pixels = new Uint32Array(leds);
