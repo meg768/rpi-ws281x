9db8e0d9aaff85e6c3e060d9c2734fb0a2171a63
meg768 <magnus@egelberg.se>
2025-10-07 21:04:03 +0200

    -

--- examples/fill-with-color.js
+++ examples/fill-with-color.js
@@ -1,20 +1,19 @@
 var ws281x = require('../index.js');
 
-
 // Configure a 13x13 matrix with serpentine mapping and gamma correction
-ws281x.configure({ width: 13, height:13, stripType: 'grb', gpio: 18, dma: 10, brightness: 255, gamma:2.2, map: 'serpentine'  });
+ws281x.configure({ width: 13, height: 13, stripType: 'grb', gpio: 18, dma: 10, brightness: 255, gamma: 2.2, map: 'serpentine' });
 
 // Create my pixels
 var pixels = new Uint32Array(leds);
 
 // Set pixels to a gradient from black to red
 for (let i = 0; i < leds; i++) {
-    let red = (i/leds) * 255;
-    let green = 0;
-    let blue = 0;
-    let color = (red << 16) | (green << 8) | blue;
-    pixels[i] = color;
-}   
+	let red = (i / leds) * 255;
+	let green = 0;
+	let blue = 0;
+	let color = (red << 16) | (green << 8) | blue;
+	pixels[i] = color;
+}
 
 // Render pixels to the Neopixel strip
 ws281x.render(pixels);
