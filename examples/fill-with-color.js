a344b259dd84c846bbf53190a3dd5b32b522308a
meg768 <magnus@egelberg.se>
2025-10-07 21:07:04 +0200

    -

--- examples/fill-with-color.js
+++ examples/fill-with-color.js
@@ -1,10 +1,14 @@
 var ws281x = require('../index.js');
 
+// Size of my matrix
+let WIDHT = 13;
+let HEIGHT = 13;
+
 // Configure a 13x13 matrix with serpentine mapping and gamma correction
-ws281x.configure({ width: 13, height: 13, stripType: 'grb', gpio: 18, dma: 10, brightness: 255, gamma: 2.2, map: 'serpentine' });
+ws281x.configure({ width: WIDTH, height: HEIGHT, stripType: 'grb', gpio: 18, dma: 10, brightness: 255, gamma: 2.2, map: 'serpentine' });
 
 // Create my pixels
-var pixels = new Uint32Array(leds);
+var pixels = new Uint32Array(WIDTH * HEIGHT  );
 
 // Set pixels to a gradient from black to red
 for (let i = 0; i < leds; i++) {
