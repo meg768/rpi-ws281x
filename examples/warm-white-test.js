09dd3a3ae55f646f81b2074269ce3741d46a4f13
meg768 <magnus@egelberg.se>
2025-10-13 23:03:05 +0200

    -

--- examples/warm-white-test.js
+++ examples/warm-white-test.js
@@ -19,7 +19,7 @@ class Example {
         // Specify RGB sequence
         this.config.stripType = 'grb';
 
-        this.config.filters = 'warm-white';
+        this.config.filters = 'warm-whitex';
 
         // Configure ws281x
         ws281x.configure(this.config);
