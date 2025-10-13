1d9d87b9baad4923210381e26a241eafe838bafb
meg768 <magnus@egelberg.se>
2025-10-13 21:29:31 +0200

    -

--- examples/test.js
+++ examples/test.js
@@ -1,7 +1,7 @@
 var ws281x = require('../index.js');
 
 // One time initialization, assumes an 8 pixel strip
-ws281x.configure({ leds: 8, transitions:'A B C' });
+ws281x.configure({ leds: 8, transitions:'warm-white white-shift' });
 
 // Create my pixels
 var pixels = new Uint32Array(8);
