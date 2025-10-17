99f05a4aa9bbe6a03f33c1590537a128d75d2547
meg768 <magnus@egelberg.se>
2025-10-17 19:08:09 +0200

    -

--- examples/test.js
+++ examples/test.js
@@ -1,5 +1,7 @@
 var ws281x = require('../index.js');
 
+/*
+
 // One time initialization, assumes an 8 pixel strip
 ws281x.configure({ leds: 8, stripType:'grbw'});
 
@@ -11,3 +13,14 @@ pixels[0] = 0xff000000;
 
 // Render pixels to the Neopixel strip
 ws281x.render(pixels);
+
+*/
+
+
+ws281x.configure({ leds: 8, stripType: 'grbw', transitions: ['RGBtoRGBW'] });
+
+pixels.fill(0);
+pixels[0] = 0x00ff0000; // röd
+pixels[1] = 0x0000ff00; // grön
+pixels[2] = 0x000000ff; // blå
+ws281x.render(pixels);  // Röd/grön/blå ska fortfarande se "röd/grön/blå" ut (ev. lite vitmix)
