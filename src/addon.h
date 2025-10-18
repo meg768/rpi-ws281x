b76a208fefcdbb800f476a609363f3e09db62662
meg768 <magnus@egelberg.se>
2025-10-19 01:43:13 +0200

    -

--- src/addon.h
+++ src/addon.h
@@ -42,6 +42,14 @@ class Addon {
 
     static config_t config;
 
+  private:
+    static inline uint8_t clamp(int value);
+    static inline void unpackWRGB(uint32_t p, uint8_t &w, uint8_t &r, uint8_t &g, uint8_t &b);
+    static void adjustColorTemperature(uint32_t *px, int n, int kelvin);
+    static void convertToRGBW(uint32_t *px, int n);
+    static uint32_t packWRGB(uint8_t w, uint8_t r, uint8_t g, uint8_t b);
+    static bool isRGBW();
+
   public:
     Addon();
 
@@ -49,13 +57,6 @@ class Addon {
     static NAN_METHOD(reset);
     static NAN_METHOD(render);
     static NAN_METHOD(sleep);
-
-    static inline uint8_t clamp(int value);
-    static inline void unpackWRGB(uint32_t p, uint8_t &w, uint8_t &r, uint8_t &g, uint8_t &b);
-    static void adjustColorTemperature(uint32_t *px, int n, int kelvin);
-    static void convertToRGBW(uint32_t *px, int n);
-    static uint32_t packWRGB(uint8_t w, uint8_t r, uint8_t g, uint8_t b);
-    static bool isRGBW();
 };
 
 #endif
