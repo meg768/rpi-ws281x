7a924adae5489a46d64d5ed2ab0d49a81c1fa437
meg768 <magnus@egelberg.se>
2025-10-18 17:57:37 +0200

    -

--- examples/warm-white-test.js
+++ examples/warm-white-test.js
@@ -23,7 +23,7 @@ class Example {
         this.config.colorTemperature = 2700;
 
         // Use RGBW
-        this.config.rgbConversion;
+        this.config.rgbConversion = true;
 
         // Configure ws281x
         ws281x.configure(this.config);
