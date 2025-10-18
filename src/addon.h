fab951f772051a3292761ceecbd7840016db673d
meg768 <magnus@egelberg.se>
2025-10-19 01:26:13 +0200

    -

--- src/addon.h
+++ src/addon.h
@@ -3,33 +3,20 @@
 #define _addon_h
 
 #include <algorithm>
-#include <dirent.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <getopt.h>
-#include <math.h>
-#include <memory.h>
+#include <cerrno>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include <nan.h>
-#include <signal.h>
 #include <stdexcept>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/ioctl.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include <string>
 #include <v8.h>
 #include <vector>
 
 extern "C" {
 #include "rpi_ws281x/ws2811.h"
 }
-
-using namespace std;
 
 class Addon {
 
