7689502dd91faf34ea6058ffab3315c8d1145f5f
meg768 <magnus@egelberg.se>
2025-10-19 16:51:40 +0200

    -

--- examples/white-test.js
+++ examples/white-test.js
@@ -20,7 +20,7 @@ class Example {
 		this.config.stripType = 'grbw';
 
 		// Set color temperature (using Kelvin)
-		this.config.colorTemperature = 40000;
+		this.config.colorTemperature = 2000;
 
         this.config.rawRGBW = true;
 
